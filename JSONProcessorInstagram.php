d199f524f3625212e1bda6cd15144f73a1142503
IMaeots <indrek160@gmail.com>
2023-12-03 19:46:39 +0200

    Instagram version BETA

--- JSONProcessorInstagram.php
+++ JSONProcessorInstagram.php
@@ -2,18 +2,178 @@
 
 class JSONProcessorInstagram
 {
-    private mixed $data;
+    private array $dictdata;
+    private array $filePaths;
+    private array $files_to_match = ["personal_information", "suggested_accounts_viewed",
+        "quizzes", "polls", "story_likes", "liked_posts", "liked_comments", "post_comments_1",
+        "reels_comments", "account_information", "saved_posts", "posts_1", "stories"];
 
 
-    public function __construct($json)
+    public function __construct(array $filePaths)
     {
-        // Take in Json data (not file!).
-        $this->data = json_decode($json, true);
+        // Take in an array of json files.
+        $this->filePaths = $filePaths;
+        $this->dictdata = ["demo" => 0];
     }
 
-
+    /**
+     * Get Instagram Data as a dictionary. You have to initialize the class with json filepaths.
+     */
     public function getInstagramDataAsDict(): array
     {
-        return array();
+        // Check if all necessary files available.
+        if (count(array_intersect($this->files_to_match, $this->filePaths)) === count($this->files_to_match)) {
+            // Start collecting data
+            foreach ($this->filePaths as $filePath) {
+                $file_name = basename($filePath);  // Extract the file name from the path.
+
+                // Check if the current file matches the desired file names.
+                if (in_array($file_name, $this->files_to_match)) {
+                    // Process the matched JSON file.
+                    $json_contents = file_get_contents($filePath);
+                    $json_data = json_decode($json_contents, true);
+
+                    // Dynamically call functions based on the file name.
+                    $this->processDataBasedOnFileName($file_name, $json_data);
+                }
+            }
+        } // If there are not enough of correct files then get the demo data.
+        else {
+            $this->dictdata = array(
+                "demo" => 1,
+                "name" => "John Doe",
+                "last_login_timestamp" => date('d/m/Y', "1641732904"),
+                "last_logout_timestamp" => date('d/m/Y', "1604074420"),
+                "first_ever_story_timestamp" => date('d/m/Y', "1497087452"),
+                "suggested_accounts_viewed_alltime" => 25,
+                "story_interactions_quizzes_current" => 8,
+                "story_interactions_polls_current" => 12,
+                "story_interactions_likes_current" => 30,
+                "post_likes_current" => 50,
+                "comment_likes_current" => 40,
+                "post_comments_alltime" => 150,
+                "reels_comments_alltime" => 75,
+                "saved_posts_thisyear" => 20,
+                "posts_thisyear" => 100,
+                "stories_thisyear" => 80
+            );
+        }
+
+        return $this->calculate_persona();
+    }
+
+    /**
+     * You must call getInstagramDataAsDict() before calling getSlideshowTexts()
+     */
+    public function getSlideshowTexts(): bool|string
+    {
+        $nameIntro = "<span class='slideshow-larger-bolder'>{$this->dictdata['name']}</span>, right?<br>Let's take a look at your activity on Instagram, shall we.";
+
+        $commentsInfo = "You have written <span class='slideshow-larger-bolder'>{$this->dictdata['post_comments_alltime']}</span> comments in Instagram<br>and this year you liked <span class='slideshow-larger-bolder'>{$this->dictdata['comment_likes_current']} comments...</span>";
+
+        $savedInfo = "You saved <span class='slideshow-x-larger-bolder'>{$this->dictdata['saved_posts_thi>syear']}</span> posts this year!<br><span class='slideshow-larger'>Whoop-Whoop!</span>";
+
+        $personaInfo = "Your TikTok persona is <span class='slideshow-larger-bolder'>{$this->dictdata['persona']}</span><br>{$this->dictdata['persona_description']}";
+
+        // CSS needs to have defined slideshow-x-larger-bolder, slideshow-larger-bolder, slideshow-x-larger, slideshow-larger.
+        return json_encode(array(
+            $nameIntro,
+            $commentsInfo,
+            $savedInfo,
+            $personaInfo
+        ));
+    }
+
+    /* Start of Private Functions. */
+    // Process data according to filename
+    private function processDataBasedOnFileName($file_name, $json_data): void
+    {
+        switch ($file_name) {
+            case 'personal_information':
+                $this->dictdata["name"] = $json_data['profile_user']['string_map_data']['Name']['value'] ?? $json_data['profile_user']['string_map_data']['Username']['value'];
+                break;
+            case 'account_information':
+                $this->dictdata["last_login_timestamp"] = date('d/m/Y', $json_data['profile_account_insights']['string_map_data']['Last Login']['timestamp']) ?? "Data missing";
+                $this->dictdata["last_logout_timestamp"] = date('d/m/Y', $json_data['profile_account_insights']['string_map_data']['Last Logout']['timestamp']) ?? "Data missing";
+                $this->dictdata["first_ever_story_timestamp"] = date('d/m/Y', $json_data['profile_account_insights']['string_map_data']['First Story Time']['timestamp']) ?? "Data missing";
+                break;
+            case 'suggested_accounts_viewed':
+                $this->dictdata["suggested_accounts_viewed_alltime"] = count($json_data['impressions_history_chaining_seen']) ?? 0;
+                break;
+            case 'quizzes':
+                $this->dictdata["story_interactions_quizzes_current"] = count($json_data['story_activities_quizzes']) ?? 0;
+                break;
+            case 'polls':
+                $this->dictdata["story_interactions_polls_current"] = count($json_data['story_activities_polls']) ?? 0;
+                break;
+            case 'story_likes':
+                $this->dictdata["story_interactions_likes_current"] = count($json_data['story_activities_story_likes']) ?? 0;
+                break;
+            case 'liked_posts':
+                $this->dictdata["post_likes_current"] = count($json_data['likes_media_likes']) ?? 0;
+                break;
+            case 'liked_comments':
+                $this->dictdata["comment_likes_current"] = count($json_data['likes_comment_likes']) ?? 0;
+                break;
+            case 'post_comments_1':
+                $this->dictdata["post_comments_alltime"] = count($json_data) ?? 0;
+                break;
+            case 'reels_comments':
+                $this->dictdata["reels_comments_alltime"] = count($json_data['comments_reels_comments']) ?? 0;
+                break;
+            case 'saved_posts':
+                $this->dictdata["saved_posts_thisyear"] = count($json_data['saved_saved_media']) ?? 0;
+                break;
+            case 'posts_1':
+                $this->dictdata["posts_thisyear"] = count($json_data) ?? 0;
+                break;
+            case 'stories':
+                $this->dictdata["stories_thisyear"] = count($json_data['ig_stories']) ?? 0;
+                break;
+            default:
+                // Extra file that came through...
+                break;
+        }
+    }
+
+    // Calculate persona from personalized data
+    private function calculate_persona(): array
+    {
+        switch (true) {
+            case ($this->dictdata['post_likes_current'] > 1000 and $this->dictdata['comment_likes_current'] > 1000 and $this->dictdata['story_interactions_likes_current'] > 100 and $this->dictdata['story_interactions_polls_current'] > 100 and $this->dictdata['story_interactions_quizzes_current'] > 100):
+                $this->dictdata['persona'] = "The Enthusiast";
+                $this->dictdata['persona_description'] = "Whatever is on Instagram, you love it!";
+                break;
+            case ($this->dictdata['posts_thisyear'] > 250 and $this->dictdata['post_likes_current'] > 800):
+                $this->dictdata['persona'] = "Nolifer";
+                $this->dictdata['persona_description'] = "Instagram loves you and you love Instagram.";
+                break;
+            case ($this->dictdata['post_likes_current'] > 1000 and $this->dictdata['comment_likes_current'] > 750 and $this->dictdata['story_interactions_likes_current'] > 200 and $this->dictdata['post_comments_alltime'] > 100):
+                $this->dictdata['persona'] = "Superman";
+                $this->dictdata['persona_description'] = "You are supportive and love to show what you like!";
+                break;
+            case ($this->dictdata['comment_likes_current'] > 50 and $this->dictdata['post_comments_alltime'] > 100):
+                $this->dictdata['persona'] = "The Charmer";
+                $this->dictdata['persona_description'] = "You write the longest and most charming comments!";
+                break;
+            case ($this->dictdata['suggested_accounts_viewed_alltime'] > 10000):
+                $this->dictdata['persona'] = "Adventurer";
+                $this->dictdata['persona_description'] = "You like to explore Instagram!";
+                break;
+            case ($this->dictdata['story_interactions_likes_current'] > 100 and $this->dictdata['story_interactions_polls_current'] > 50 and $this->dictdata['story_interactions_quizzes_current'] > 10):
+                $this->dictdata['persona'] = "Lifestyle admirer";
+                $this->dictdata['persona_description'] = "You like to keep an eye on your favourites!";
+                break;
+            case ($this->dictdata['post_likes_current'] < 25 and $this->dictdata['comment_likes_current'] < 10 and $this->dictdata['post_comments_alltime'] < 20):
+                $this->dictdata['persona'] = "Lurker";
+                $this->dictdata['persona_description'] = "You like to scroll without interacting!";
+                break;
+            default:
+                    $this->dictdata['persona'] = "Chiller";
+                    $this->dictdata['persona_description'] = "You just like to chill!";
+                    break;
+            }
+
+        return $this->dictdata;
     }
 }
--- tiktok-analytics-plugin.php
+++ tiktok-analytics-plugin.php
@@ -8,8 +8,11 @@ Author: IMaeots
 
 // Include your model.php file
 require_once(plugin_dir_path(__FILE__) . 'JSONProcessorTikTok.php');
+require_once(plugin_dir_path(__FILE__) . 'JSONProcessorInstagram.php');
 
-// Helper function.
+/*
+ * Helper functions that help :)
+ */
 function display_error_msg($message): void
 {
     ?>
@@ -19,9 +22,127 @@ function display_error_msg($message): void
     <?php
 }
 
+function findJSONFiles($dir): array
+{
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+    $jsonFiles = [];
+    foreach ($iterator as $file) {
+        if ($file->isFile() && $file->getExtension() === 'json') {
+            $jsonFiles[] = $file->getPathname();
+        }
+    }
+    return $jsonFiles;
+}
 
-// Hook into WordPress action for handling file uploads
-add_action('init', 'handle_tiktok_upload');
+// Remove the extracted directory and its contents
+function rrmdir($dir): void
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir.'/'.$object)) {
+                    rrmdir($dir.'/'.$object);
+                } else {
+                    unlink($dir.'/'.$object);
+                }
+            }
+        }
+        rmdir($dir);
+    }
+}
+
+// Main Action for file uploads.
+add_action('init', 'handle_upload_actions');
+function handle_upload_actions() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_POST['action'] === 'handle_instagram_upload') {
+            handle_instagram_upload();
+        } elseif ($_POST['action'] === 'handle_tiktok_upload') {
+            handle_tiktok_upload();
+        }
+    }
+}
+
+
+// Lower level action handles for file uploads.
+function handle_instagram_upload(): void
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['zip_file'])) {
+        $file = $_FILES['zip_file'];
+
+        // File size check
+        $maxFileSize = 20 * 1024 * 1024; // 20 megabytes
+        if ($file['size'] > $maxFileSize) {
+            $msg = "File size exceeds the allowed limit. Maximum file size is 20MB.";
+            // Display error message
+            display_error_msg($msg);
+        }
+
+        // Check if a file was uploaded
+        if ($file['error'] === UPLOAD_ERR_OK) {
+            $zip = new ZipArchive;
+
+            // Path to the uploaded zip file
+            $zipFilePath = $_FILES['zip_file']['tmp_name'];
+
+            // Directory where you want to extract the zip contents
+            $extractPath = 'extracted/';
+
+            // Create the extraction directory if it doesn't exist
+            if (!file_exists($extractPath)) {
+                mkdir($extractPath, 0777, true);
+            }
+
+            // Open the zip file
+            if ($zip->open($zipFilePath) === TRUE) {
+                // Extract the contents of the zip file
+                $zip->extractTo($extractPath);
+                $zip->close();
+
+                // Find JSON files within directories
+                $jsonFiles = findJSONFiles($extractPath);
+                // Give JSON files to JSONProcessor and get processed data back
+                $jsonProcessor = new JSONProcessorInstagram($jsonFiles);
+                $dictData = $jsonProcessor->getInstagramDataAsDict();
+                $slideshowTexts = $jsonProcessor->getSlideshowTexts();
+
+                // Delete user input zip content
+                rrmdir($extractPath);
+
+                // Start a session, input data, redirect.
+                session_start();
+                $_SESSION['data'] = $dictData;
+                $_SESSION['data_for_slideshow_json'] = $slideshowTexts;
+
+                wp_redirect(site_url('/instagram-wrapped'));
+                exit();
+
+
+            } else {
+                echo 'Failed to open the zip file';
+            }
+        } else {
+            echo 'Error uploading zip';
+        }
+
+
+    } elseif (isset($_POST['start_demo'])) {
+        // Make fake data, input into session, and redirect.
+        $jsonProcessor = new JSONProcessorInstagram([""]);
+        $dictData = $jsonProcessor->getInstagramDataAsDict();
+        $slideshowTexts = $jsonProcessor->getSlideshowTexts($dictData);
+
+        session_start();
+        $_SESSION['data'] = $dictData;
+        $_SESSION['data_for_slideshow_json'] = $slideshowTexts;
+
+        wp_redirect(site_url('/instagram-wrapped'));
+        exit();
+    }
+}
+
+
 function handle_tiktok_upload(): void
 {
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['jsonFile'])) {
@@ -130,19 +251,12 @@ function handle_tiktok_upload(): void
 }
 
 
-add_action('init', 'handle_instagram_upload');
-function handle_instagram_upload(): void
-{
-
-}
-
-add_action('init', 'handle_facebook_upload');
 function handle_facebook_upload(): void
 {
 
 }
 
-add_action('init', 'handle_x_upload');
+
 function handle_x_upload(): void
 {
 
